46b81089ef3593884dcf17ec70432427ba131a08
Adam Measel <ameasel55@gmail.com>
2023-01-31 10:04:25 -0600

    frank

---
+++ VeryGoodYear/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+string goodYear = "very good year";
+int firstNumber = 17;
+int secondNumber = 21;
+int thirdNumber = 35;
+
+Console.WriteLine($"When I was {firstNumber} it was a {goodYear}");
+Console.WriteLine($"It was a {goodYear} for small town girls and soft summer nights");
+Console.WriteLine($"We'd hide from the lights on the village green");
+Console.WriteLine($"When I was {firstNumber}");
+Console.WriteLine($"");
+Console.WriteLine($"When I was {secondNumber} it was a {goodYear}");
+Console.WriteLine($"It was a {goodYear} for city girls who lived up the stair");
+Console.WriteLine($"With all that perfumed hair and it came undone");
+Console.WriteLine($"When I was {secondNumber}");
+Console.WriteLine($"");
+Console.WriteLine($"Then when I was {thirdNumber} it was a {goodYear}");
+Console.WriteLine($"It was a {goodYear} for blue-blooded girls");
+Console.WriteLine($"Of independent means, we'd ride in limousines their chauffers would drive");
+Console.WriteLine($"When I was {thirdNumber}");
+Console.WriteLine($"");
+Console.WriteLine($"But now the days are short, I'm in the autumn of the year");
+Console.WriteLine($"And now I think of my life as vintage wine from fine old kegs");
+Console.WriteLine($"From the brim to the dregs, and it poured sweet and clear");
+Console.WriteLine($"It was a {goodYear}");
+Console.WriteLine($"");
+Console.WriteLine($"It was a mess of good years");
+
+
+
+
